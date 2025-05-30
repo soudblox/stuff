c679b6a9a0d97f2c86c64565e9ddcaab3aa02e36
soudblox <nick8x@gmail.com>
2025-05-30 17:03:36 +0700

    fix highlighting on firefox and update spotify

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const siteData = {
 							title: "Spotify Premium",
 							description: "Free Spotify Premium",
 							image: "https://primordialradio.com/wp-content/uploads/2024/04/Link_Image_Spotify.jpg",
-							link: "https://github.com/soudblox/nichdant-revanced/releases/download/6/spotify-revanced_v9.0.46.496-patches_v5.24.0.2.apk",
+							link: "https://github.com/soudblox/nichdant-revanced/releases/download/new/spotify-revanced_v9.0.48.380-patches_v5.25.0.1.apk",
 							source: "ReVanced Team",
 							sourceUrl: "https://github.com/ReVanced"
 						},
